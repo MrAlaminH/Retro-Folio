192dd5dfb134ba8246fdb1945407fc0884cfe6da
Its-Alamin-H <alaminhosssain97@gmail.com>
2024-09-03 01:31:41 +0600

    analyticsAdded

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,10 +4,28 @@ import localFont from "next/font/local";
 import Navbar from "@/components/NavBar";
 import Footer from "@/components/Footer";
 import MatrixCursor from "@/components/MatrixCursor/MatrixCursor";
+import { PHProvider } from "./providers";
+import dynamic from "next/dynamic";
+
+const PostHogPageView = dynamic(() => import("./PostHogPageView"), {
+  ssr: false,
+});
 
 export const metadata: Metadata = {
-  title: "MrAlaminH",
-  description: "Alamin's Worls",
+  metadataBase: new URL("https://uralamin.is-a.dev/"),
+  title: {
+    default: "MrAlaminH: Code, Create, Innovate",
+    template:
+      "A showcase of MrAlaminH journey | I’m on a journey to blend creativity with technology.",
+  },
+  description:
+    "A showcase of MrAlaminH journey | I’m on a journey to blend creativity with technology.",
+  twitter: {
+    card: "summary_large_image",
+  },
+  openGraph: {
+    images: "/opengraph-image.png",
+  },
 };
 
 const ibmBios = localFont({ src: "../public/fonts/WebPlus_IBM_BIOS.woff" });
@@ -19,12 +37,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${ibmBios.className} bg-black`}>
-        <Navbar />
-        {children}
-        <MatrixCursor />
-        <Footer />
-      </body>
+      <PHProvider>
+        <body className={`${ibmBios.className} bg-black`}>
+          <PostHogPageView />
+          <Navbar />
+          {children}
+          <MatrixCursor />
+          <Footer />
+        </body>
+      </PHProvider>
     </html>
   );
 }
